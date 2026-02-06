286ef82685d2ad70516f555cbd2515fbd20b9e6d
LearnPRG-py <aryankrishnan4b@gmail.com>
2026-02-06 21:56:29 +0530

    Update checkAnswers.js

--- Challenge 5/netlify/functions/checkAnswers.js
+++ Challenge 5/netlify/functions/checkAnswers.js
@@ -1,22 +1,22 @@
-const answers = ["ow","or","ld","ll","he"];
+const answers = ["ow", "or", "ld", "ll", "he"];
 
 export async function handler(event) {
   const { challenge, answer } = JSON.parse(event.body);
 
-  if (challenge === "access") {
+  if (challenge == 5) {
     if (answer.toLowerCase() === "hello world") {
       return {
         statusCode: 200,
         body: JSON.stringify({
           success: true,
-          url: "https://hilarious-taiyaki-c278ae.netlify.app"
-        })
+          url: "https://hilarious-taiyaki-c278ae.netlify.app",
+        }),
       };
     }
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ success: false })
+      body: JSON.stringify({ success: false }),
     };
   }
 
@@ -24,6 +24,6 @@ export async function handler(event) {
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ success: correct })
+    body: JSON.stringify({ success: correct }),
   };
 }
