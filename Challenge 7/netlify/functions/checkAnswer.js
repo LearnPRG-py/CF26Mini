b1c7c46b69a680b8a58317d5bc1501db5906a8a0
LearnPRG-py <aryankrishnan4b@gmail.com>
2026-02-06 16:14:10 +0530

    Add netlify function to C7

---
+++ Challenge 7/netlify/functions/checkAnswer.js
@@ -0,0 +1,21 @@
+
+export async function handler(event, context) {
+  const { answer } = JSON.parse(event.body);
+  if (answer.trim() === "12") {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true,
+        url: "https://github.com/LearnPRG-py/EpikCodefestIdeaz",
+      }),
+    };
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: false,
+        message: "Incorrect line number.",
+      }),
+    };
+  }
+}
