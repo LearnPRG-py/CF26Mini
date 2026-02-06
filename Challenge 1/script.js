d840a8bfc6c1e3813caef62a56e13b0e46024154
MaanBizzzz <maanbiswasis@gmail.com>
2026-02-06 18:19:46 +0530

    ch1,ch2

---
+++ Challenge 1/script.js
@@ -0,0 +1,23 @@
+function submitValue() {
+  const val = document.getElementById("numInput").value;
+  const status = document.getElementById("status");
+  const congrats = document.getElementById("congrats");
+  const nextBtn = document.getElementById("nextBtn");
+
+  if (val === "") {
+    status.textContent = "Enter a number first.";
+    congrats.style.display = "none";
+    nextBtn.style.display = "none";
+    return;
+  }
+
+  const expected = 43; // If they find this its fine cos its basically the same as finding the bug
+  const numeric = Number(val);
+
+  if (Number.isNaN(numeric)) {
+    status.textContent = "That is not a valid number.";
+    congrats.style.display = "none";
+    nextBtn.style.display = "none";
+    return;
+  }
+}
